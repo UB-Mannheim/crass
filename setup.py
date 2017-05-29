3c09f907ed6e6ef198d77ba63ef588e5399474fd
jkamlah <jan.kamlah@gmail.com>
2017-05-29 09:59:50 +0200

    --starting a fresh repo with the new
    Signed-off-by: jkamlah <jan.kamlah@gmail.com>

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='crass',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='jkamlah',
+    author_email='',
+    description=''
+)
